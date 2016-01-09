3e8100198974805c10edfe010a05899749d69512
michael-o0 <michael.burge77@gmail.com>
2016-01-09 00:55:20 -0800

    ;

--- App.cpp
+++ App.cpp
@@ -55,108 +55,10 @@ int main(int argc, char* args[]) {
 		}
 	}
 
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		std::cout << "VIDEONoGO." << std::endl << SDL_GetError() << std::endl;
-		return -1;
-	}
-
-	atexit(SDL_Quit);
+	GFX::Init();
 
 //	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "C", "H", NULL);
 
-//	#ifdef DEBUG_SDL
-	SDL_LogSetAllPriority(SDL_LOG_PRIORITY_WARN);
-//	#endif
-
-	SDL_DisplayMode currentWindow;
-
-//	int numVideoDisplays = SDL_GetNumVideoDisplays();
-
-	if (SDL_GetCurrentDisplayMode(0, &currentWindow) == 0) {
-		int _hor_scale_factor;
-		int _ver_scale_factor;
-
-		_hor_scale_factor = ((currentWindow.w / GFX::base_width) / 2) * 2;
-		std::cout << _hor_scale_factor << std::endl;
-
-		_ver_scale_factor = ((currentWindow.h / GFX::base_height) / 2) * 2;
-		std::cout << _ver_scale_factor << std::endl;
-
-		if (_ver_scale_factor < _hor_scale_factor) {
-			GFX::scale_factor = _ver_scale_factor;
-		} else if (_ver_scale_factor > _hor_scale_factor) {
-			GFX::scale_factor = _hor_scale_factor;
-		} else {
-			GFX::scale_factor = _hor_scale_factor;
-		}
-	} else {
-		SDL_LogMessage(SDL_LOG_CATEGORY_VIDEO, SDL_LOG_PRIORITY_WARN, "Could not get CurrentDisplayMode, Going with a GFX scale factor of 2");
-		GFX::scale_factor = 2;
-	}
-
-	//Set the scaling quality to nearest-pixel
-	if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "0"))
-	{
-		SDL_LogMessage(SDL_LOG_CATEGORY_RENDER, SDL_LOG_PRIORITY_WARN, "Could not Set Render Scale Quality");
-	}
-
-	SDL_Window *window_AppWindow = nullptr;
-	window_AppWindow = SDL_CreateWindow("Civ",
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		GFX::base_width * GFX::scale_factor,
-		GFX::base_height * GFX::scale_factor,
-		SDL_WINDOW_SHOWN
-		);
-	if (window_AppWindow == nullptr) {
-		SDL_LogMessage(SDL_LOG_CATEGORY_VIDEO, SDL_LOG_PRIORITY_CRITICAL, "Could not Create AppWindow");
-		return -1;
-	}
-
-	SDL_Renderer *renderer_Renderer = nullptr;
-	renderer_Renderer = SDL_CreateRenderer(window_AppWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
-	if (renderer_Renderer == nullptr) {
-		SDL_LogMessage(SDL_LOG_CATEGORY_RENDER, SDL_LOG_PRIORITY_CRITICAL, "Could not Create Rederer");
-		return -1;
-	}
-/*
-	SDL_Window *window_TextureRenderWindow = nullptr;
-	window_TextureRenderWindow = SDL_CreateWindow("TextureRenderWindow",
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		App::Video.window_base_width,// * App::Video.window_scale_factor,
-		App::Video.window_base_height,// * App::Video.window_scale_factor,
-		SDL_WINDOW_HIDDEN
-		);
-	if (window_TextureRenderWindow == nullptr) {
-		SDL_LogMessage(SDL_LOG_CATEGORY_VIDEO, SDL_LOG_PRIORITY_CRITICAL, "Could not Create TextureRenderWindow");
-		return -1;
-	}
-
-	SDL_Renderer *renderer_TextureRenderer = nullptr;
-	renderer_TextureRenderer = SDL_CreateRenderer(window_TextureRenderWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
-	if (renderer_TextureRenderer == nullptr) {
-		SDL_LogMessage(SDL_LOG_CATEGORY_RENDER, SDL_LOG_PRIORITY_CRITICAL, "Could not Create TextureRenderer");
-		return -1;
-	}
-*/
-	SDL_RenderClear(renderer_Renderer);
-
-	//Set so it's noticeable if it doesn't come out right.
-	SDL_SetRenderDrawColor(renderer_Renderer, 0, 0, 0, 0);
-
-	//Similarly, you must use SDL_TEXTUREACCESS_TARGET when you create the texture
-/*	SDL_Texture *backBuffer = NULL;
-	backBuffer = SDL_CreateTexture(renderer, SDL_GetWindowPixelFormat(window), SDL_TEXTUREACCESS_TARGET, 320, 200);
-	if (backBuffer == NULL) {
-		SDL_DestroyRenderer(renderer);
-		SDL_DestroyWindow(window);
-		return -1;
-	}
-
-	//IMPORTANT Set the back buffer as the target
-	SDL_SetRenderTarget(renderer, backBuffer);
-*/
 	App::Options.SMsCd = "/Volumes/contn/deviceuser/Downloads/Game/civ/";
 
 	std::vector<PICImage> PICs(10);//CivPicFiles.size());
@@ -167,12 +69,12 @@ int main(int argc, char* args[]) {
 	}
 
 	for (int iter = 0; iter < PICImageTextures.size(); ++iter) {
-		GFX::Create256ColourPICImageTexture(&PICs[iter], &PICImageTextures[iter], renderer_Renderer);
+		GFX::Create256ColourPICImageTexture(&PICs[iter], &PICImageTextures[iter], GFX::renderer_GFXRenderer);
 	}
 
 	int ti = 0;
 
-	GFX::RenderPICImageTexture(PICs, PICImageTextures, ti, renderer_Renderer);
+	GFX::RenderPICImageTexture(PICs, PICImageTextures, ti, GFX::renderer_GFXRenderer);
 
 	SDL_Event sdl_event;
 	bool quit = false;
@@ -192,7 +94,7 @@ int main(int argc, char* args[]) {
 							break;
 					}
 					if (PICImageTextures[ti].PICImageTexture_datap != nullptr) {
-							GFX::RenderPICImageTexture(PICs, PICImageTextures, ti, renderer_Renderer);
+							GFX::RenderPICImageTexture(PICs, PICImageTextures, ti, GFX::renderer_GFXRenderer);
 					}
 					break;
 			}
@@ -213,18 +115,7 @@ int main(int argc, char* args[]) {
 		}
 	}
 
-//	SDL_DestroyRenderer(renderer_TextureRenderer);
-//	renderer_TextureRenderer = nullptr;
-
-//	SDL_DestroyWindow(window_TextureRenderWindow);
-//	window_TextureRenderWindow = nullptr;
-
-
-	SDL_DestroyRenderer(renderer_Renderer);
-	renderer_Renderer = nullptr;
-
-	SDL_DestroyWindow(window_AppWindow);
-	window_AppWindow = nullptr;
+	GFX::Quit();
 
 	std::cout << " Bye.\n";
 
--- GFX.hpp
+++ GFX.hpp
@@ -36,9 +36,91 @@ struct PICImageTexture {
 };
 
 namespace GFX {
+	SDL_Window *window_GFXWindow = nullptr;
+	SDL_Renderer *renderer_GFXRenderer = nullptr;
+
 	const int base_width = 320;
 	const int base_height = 200;
 	int scale_factor;
+
+	void Quit() {
+		SDL_DestroyRenderer(renderer_GFXRenderer);
+		renderer_GFXRenderer = nullptr;
+
+		SDL_DestroyWindow(window_GFXWindow);
+		window_GFXWindow = nullptr;
+
+		SDL_Quit();
+	}
+
+	int Init () {
+		if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+			std::cerr << "VIDEONoGO." << std::endl << SDL_GetError() << std::endl;
+			return -1;
+		}
+
+		atexit(GFX::Quit);
+
+//		#ifdef DEBUG_SDL
+		SDL_LogSetAllPriority(SDL_LOG_PRIORITY_WARN);
+//		#endif
+
+//		int numVideoDisplays = SDL_GetNumVideoDisplays();
+
+		SDL_DisplayMode displayMode_CurrentDisplayMode;
+
+		if (SDL_GetCurrentDisplayMode(0, &displayMode_CurrentDisplayMode) == 0) {
+			int _hor_scale_factor;
+			int _ver_scale_factor;
+
+			_hor_scale_factor = ((displayMode_CurrentDisplayMode.w / GFX::base_width) / 2) * 2;
+			std::cout << _hor_scale_factor << std::endl;
+
+			_ver_scale_factor = ((displayMode_CurrentDisplayMode.h / GFX::base_height) / 2) * 2;
+			std::cout << _ver_scale_factor << std::endl;
+
+			if (_ver_scale_factor < _hor_scale_factor) {
+				GFX::scale_factor = _ver_scale_factor;
+			} else if (_ver_scale_factor > _hor_scale_factor) {
+				GFX::scale_factor = _hor_scale_factor;
+			} else {
+				GFX::scale_factor = _hor_scale_factor;
+			}
+		} else {
+			SDL_LogMessage(SDL_LOG_CATEGORY_VIDEO, SDL_LOG_PRIORITY_WARN, "Could not get CurrentDisplayMode, Going with a GFX scale factor of 2");
+			GFX::scale_factor = 2;
+		}
+
+		//Set the scaling quality to nearest-pixel
+		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "0"))
+		{
+			SDL_LogMessage(SDL_LOG_CATEGORY_RENDER, SDL_LOG_PRIORITY_WARN, "Could not Set Render Scale Quality");
+		}
+
+		window_GFXWindow = SDL_CreateWindow("Civ",
+			SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED,
+			GFX::base_width * GFX::scale_factor,
+			GFX::base_height * GFX::scale_factor,
+			SDL_WINDOW_SHOWN
+			);
+		if (window_GFXWindow == nullptr) {
+			SDL_LogMessage(SDL_LOG_CATEGORY_VIDEO, SDL_LOG_PRIORITY_CRITICAL, "Could not Create Window");
+			return -1;
+		}
+
+		renderer_GFXRenderer = SDL_CreateRenderer(window_GFXWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
+		if (renderer_GFXRenderer == nullptr) {
+			SDL_LogMessage(SDL_LOG_CATEGORY_RENDER, SDL_LOG_PRIORITY_CRITICAL, "Could not Create Renderer");
+			return -1;
+		}
+
+		SDL_SetRenderDrawColor(renderer_GFXRenderer, 0, 0, 0, 0);
+
+		SDL_RenderClear(renderer_GFXRenderer);
+
+		return 0;
+	}
 
 	int RenderPICImageTexture(std::vector<PICImage> PICs, std::vector<PICImageTexture> PICImageTextures, int ti, SDL_Renderer *renderer_Renderer) {
 
@@ -271,5 +353,46 @@ std::cout << __PICImage->picimage_fullfilepath << " " << __PICImage->picimage_16
 		return 0;
 	}
 }
+
+/*
+	SDL_Window *window_TextureRenderWindow = nullptr;
+	window_TextureRenderWindow = SDL_CreateWindow("TextureRenderWindow",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		App::Video.window_base_width,// * App::Video.window_scale_factor,
+		App::Video.window_base_height,// * App::Video.window_scale_factor,
+		SDL_WINDOW_HIDDEN
+		);
+	if (window_TextureRenderWindow == nullptr) {
+		SDL_LogMessage(SDL_LOG_CATEGORY_VIDEO, SDL_LOG_PRIORITY_CRITICAL, "Could not Create TextureRenderWindow");
+		return -1;
+	}
+
+	SDL_Renderer *renderer_TextureRenderer = nullptr;
+	renderer_TextureRenderer = SDL_CreateRenderer(window_TextureRenderWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
+	if (renderer_TextureRenderer == nullptr) {
+		SDL_LogMessage(SDL_LOG_CATEGORY_RENDER, SDL_LOG_PRIORITY_CRITICAL, "Could not Create TextureRenderer");
+		return -1;
+	}
+*/
+
+	//Similarly, you must use SDL_TEXTUREACCESS_TARGET when you create the texture
+/*	SDL_Texture *backBuffer = NULL;
+	backBuffer = SDL_CreateTexture(renderer, SDL_GetWindowPixelFormat(window), SDL_TEXTUREACCESS_TARGET, 320, 200);
+	if (backBuffer == NULL) {
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		return -1;
+	}
+
+	//IMPORTANT Set the back buffer as the target
+	SDL_SetRenderTarget(renderer, backBuffer);
+*/
+
+//	SDL_DestroyRenderer(renderer_TextureRenderer);
+//	renderer_TextureRenderer = nullptr;
+
+//	SDL_DestroyWindow(window_TextureRenderWindow);
+//	window_TextureRenderWindow = nullptr;
 
 #endif /* !GFX_HPP */
